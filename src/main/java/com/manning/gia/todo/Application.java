54c43ae17f16c3e85c899c40f53c1082c3085805
Abhi <abhimanyu.singhal@live.com>
2020-08-12 17:40:23 +0530

    Update Application.java

--- src/main/java/com/manning/gia/todo/Application.java
+++ src/main/java/com/manning/gia/todo/Application.java
@@ -1,5 +1,5 @@
 package com.manning.gia.todo;
-
+/* Code */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
